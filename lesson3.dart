26dcd1856145929e5467451c15d1e6f8e1187c66
chikaasan <chyngyz.asanhodjoevv@gmail.com>
2021-03-16 10:49:21 +0300

    home work3

---
+++ lesson3.dart
@@ -0,0 +1,15 @@
+
+main() { ///main function
+  
+  // variables
+  var a = 25;
+  
+  int b = 35;
+  
+  double c = 45;
+
+  
+  /* a function that shows the sum of 
+  our variables */
+  print(a+b+c);
+}
